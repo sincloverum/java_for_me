0fd461884e9dd459bcf345a87c9a1658d558322f
X5G814 <chromis.biz@gmail.com>
2022-01-29 05:45:59 +0200

    Начальное заполнение тестового задания для учёбы на ноутбуке.

---
+++ src/Calc.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public String calc(String inputString) {
+        //Твой код здесь
+        return null;
+    }
+}
+
+//Ты так же можешь добавлять свои классы (этот класс, если не потребуется, можно удалить)
+class CalculatorHelper {
+
+}
